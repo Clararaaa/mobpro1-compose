22eaa0f635c5ad9c70edcd4e60bed086a15a2930
Clararaaa <clara01angelica@gmail.com>
2026-02-21 21:39:06 +0700

    Membuat database dengan Room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,6 +10,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.devtools.ksp)
 }
 
 android {
@@ -60,6 +61,9 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.compose.material.icons.extended)
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
