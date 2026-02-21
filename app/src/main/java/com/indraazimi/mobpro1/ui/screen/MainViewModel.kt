de072010b6a93a31bf35f203818b2a1d97ca9d08
Clararaaa <clara01angelica@gmail.com>
2026-02-21 23:00:28 +0700

    Mengambil data dari database

--- app/src/main/java/com/indraazimi/mobpro1/ui/screen/MainViewModel.kt
+++ app/src/main/java/com/indraazimi/mobpro1/ui/screen/MainViewModel.kt
@@ -10,63 +10,23 @@
 package com.indraazimi.mobpro1.ui.screen
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.indraazimi.mobpro1.database.CatatanDao
 import com.indraazimi.mobpro1.model.Catatan
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.WhileSubscribed
+import kotlinx.coroutines.flow.stateIn
 
-class MainViewModel : ViewModel() {
+class MainViewModel(dao: CatatanDao) : ViewModel() {
 
-    val data = listOf(
-        Catatan(
-            1,
-            "Kuliah Mobpro 23 Feb",
-            "Kuliah hari pertama. Ternyata keren juga yang mau dipelajari.",
-            "2026-02-23 12:34:56"
-        ),
-        Catatan(
-            2,
-            "Kuliah Mobpro 25 Feb",
-            "Praktikum pertama: running modul, Alhamdulillah hari ini lancar.",
-            "2026-02-25 12:34:56"
-        ),
-        Catatan(
-            3,
-            "Kuliah Mobpro 02 Mar",
-            "Kuliah hari kedua. Ternyata cukup mudah di pahami",
-            "2026-03-02 12:34:56"
-        ),
-        Catatan(
-            4,
-            "Kuliah Mobpro 04 Mar",
-            "Praktikum Kedua. Sangat lancar dan semakin semangat untuk belajar",
-            "2026-03-04 12:34:56"
-        ),
-        Catatan(
-            5,
-            "Kuliah Mobpro 09 Mar",
-            "Kuliah selanjutnya. Banyak hal baru yang dipelajari",
-            "2026-03-09 12:34:56"
-        ),
-        Catatan(
-            6,
-            "Kuliah Mobpro 11 Maret",
-            "Praktikum kali ini membuat aplikasi Galeri Hewan.." +
-            "Klik tombol lanjut, maka foto dan nama hewannya berubah.",
-            "2026-03-11 12:34:56"
-        ),
-        Catatan(
-            7,
-            "Kuliah Mobpro 30 Mar",
-            "Kuliah hari ini. Ternyata semakin asik",
-            "2026-03-30 12:34:56"
-        ),
-        Catatan(
-            8,
-            "Kuliah Mobpro 01 April",
-            "Praktikum kali ini membuat aplikasi perhitungan BMI",
-            "2026-04-01 12:34:56"
-        )
+    val data: StateFlow<List<Catatan>> = dao.getCatatan().stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(),
+        initialValue = emptyList()
     )
 
     fun getCatatan(id: Long): Catatan? {
-        return data.find { it.id == id}
+        return data.value.find { it.id == id}
     }
 }
